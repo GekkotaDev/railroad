656c10ee6caf30872f706a61cfa442335f36b3d4
gekkotadev <114091010+GekkotaDev@users.noreply.github.com>
2024-06-13 19:00:11 +0800

    Aliases

--- web/app/nuxt.config.ts
+++ web/app/nuxt.config.ts
@@ -1,7 +1,14 @@
+import { fileURLToPath } from "node:url"
+
 import Defaults from "./.railroad/nuxt-config"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ...Defaults,
+
+  alias: {
+    "#!": fileURLToPath(new URL("./macros", import.meta.url)),
+    workers: fileURLToPath(new URL("./assets/workers", import.meta.url)),
+  },
 })
 
