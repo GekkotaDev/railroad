71ec93bc13211f0c942e688e910cc56a01c16979
gekkotadev <114091010+GekkotaDev@users.noreply.github.com>
2024-06-13 19:01:20 +0800

    Components

---
+++ web/app/components/Titlebar/TitlebarLeaf.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { TitlebarLeafProps } from "#imports"
+
+$defineProps<TitlebarLeafProps>()
+</script>
+
+<template>
+  <UiMenubarItem :disabled @select="action">
+    {{ label }}
+    <UiMenubarShortcut v-if="shortcut"> {{ shortcut }} </UiMenubarShortcut>
+  </UiMenubarItem>
+</template>
+
---
+++ web/app/components/Window/Zoom/WindowZoomUniversal.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { useWindowSize } from "@vueuse/core"
+
+type Props = {
+  onMaximize: () => void
+  onRestore: () => void
+}
+
+const { width } = useWindowSize()
+const state = $computed(() => {
+  if (
+    width.value === screen.availWidth - 1 ||
+    width.value === screen.availWidth ||
+    width.value === screen.availWidth + 1
+  )
+    return "restore"
+  return "maximize"
+})
+
+$defineProps<Props>()
+</script>
+
+<template>
+  <div
+    aria-hidden
+    class="flex h-full w-12 items-center justify-center transition hover:bg-neutral-500/20"
+    @click="
+      () =>
+        match(state)
+          .with('restore', () => onRestore())
+          .with('maximize', () => onMaximize())
+          .exhaustive()
+    "
+  >
+    <Icon
+      v-show="state === 'restore'"
+      name="material-symbols:chrome-restore-outline-rounded"
+    />
+    <Icon
+      v-show="state === 'maximize'"
+      name="material-symbols:chrome-maximize-outline"
+    />
+  </div>
+</template>
+
